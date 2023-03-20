5143c1e3d5762f02077a322e63794bf28d129f3d
Marcos Morais <marcosmorais.v10@gmail.com>
2023-03-19 22:25:22 -0300

    project added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
 
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+  get "/home", to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
